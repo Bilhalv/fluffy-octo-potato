12f93485e2d525c0ac2d47dc448d1f777c78cc2f
Bilhalv Casa <pedrokabilhalva@gmail.com>
2024-02-12 19:06:48 -0300

    chore: removed unused comment

--- src/components/Gerais/ToastComponent.tsx
+++ src/components/Gerais/ToastComponent.tsx
@@ -1,4 +1,3 @@
-// add a mui component that can be stackable on a column, like the chackaui toast
 import React from "react";
 import { Bounce, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
